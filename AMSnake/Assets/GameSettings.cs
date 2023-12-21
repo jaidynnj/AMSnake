2c87a6b7c47ae3769d67dde4f0731598d30044a4
61617774CTC <61617774CTC@MA-CTC-SDD-S15>
2023-12-21 09:53:17 -0500

    Add project files.

---
+++ AMSnake/Assets/GameSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMSnake.Assets
+{
+    public static class GameSettings
+    {
+        public static int ShakeDuration { get; set; } = 2000;
+        public static int boostSpeed { get; set; } = 100;
+        public static double WallDensity { get; set; } = .15;
+        public static bool WallFatality { get; set; } = true;
+    }
+}
---
+++ AMSnake/MainWindow.xaml.cs
@@ -0,0 +1,251 @@
+﻿using AMSnake.Assets;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace AMSnake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            {GridValue.Empty, Images.Empty },
+            {GridValue.Snake, Images.Body },
+            {GridValue.Food, Images.Food },
+             {GridValue.Walls, Images.Walls },
+        };
+
+        private readonly Dictionary<Direction, int> dirToRotation = new()
+        {
+            {Direction.Up, 0},
+            {Direction.Right, 90 },
+            {Direction.Down, 180 },
+            {Direction.Left, 270 }
+        };
+
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+        private GameState gameState;
+        private bool gameRunning;
+        private List<int> highScores = new List<int>();
+        private int boostSpeed = 0;
+        private Random random = new Random();
+        private DispatcherTimer boostTimer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+            boostTimer.Interval = TimeSpan.FromSeconds(1);
+            boostTimer.Tick += BoostTimer_Tick;
+        }
+
+        private void BoostTimer_Tick(object sender, EventArgs e)
+        {
+            boostSpeed = 0;
+        }
+
+        private async Task RunGame()
+        {
+            Draw();
+            await ShowCountDown();
+            Overlay.Visibility = Visibility.Hidden;
+            await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible) 
+            {
+             e.Handled = true;
+            }
+
+            if (!gameRunning) 
+            { 
+              gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            } 
+            
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameState.GameOver)
+            {
+                return;
+            }
+
+
+            switch (e.Key)
+            {
+                case Key.Left:
+                    gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right:
+                    gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up:
+                    gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down:
+                    gameState.ChangeDirection(Direction.Down);
+                    break;
+                case Key.Space:
+                    boostSpeed= (boostSpeed == 0) ? GameSettings.boostSpeed : 0;
+                    boostTimer.Start();
+                    break;
+
+                
+            }
+        }
+
+        private async Task GameLoop()
+        {
+            while(!gameState.GameOver)
+            {
+                await Task.Delay(200 - boostSpeed);
+                gameState.Move();
+                Draw();
+            }
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+            GameGrid.Width = GameGrid.Height * ( cols / (double)rows);
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            
+            return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+            DrawSnakeHead();
+            ScoreText.Text = $"SCORE {gameState.Score}";
+        }
+
+        private void DrawGrid()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                    gridImages[r, c].RenderTransform = Transform.Identity;
+                }
+            }
+        }
+
+        private void DrawSnakeHead()
+        {
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+
+            int roatation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(roatation);
+           
+        }
+
+        private async Task DrawDeadSnake() 
+        { 
+           List<Position> positions = new List<Position>(gameState.SnakePositions());
+            
+            for (int i = 0; i < positions.Count; i++) 
+            {
+                Position pos = positions[i];
+                ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
+                 gridImages[pos.Row, pos.Col].Source = source;
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task ShowCountDown()
+        {
+            for (int i = 3; i >= 1; i --)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
+            }
+        }
+
+        private async Task ShowGameOver()
+        {
+            boostSpeed = 0;
+            ShakeWindow(GameSettings.ShakeDuration);
+            await DrawDeadSnake();
+            await Task.Delay(1000);
+
+            OverlayText.Text = "PRESS ANY KEY START \n LEADERBOARD";
+            UpdateLeaderboard();
+            Overlay.Visibility = Visibility.Visible;
+           
+        }
+        private void UpdateLeaderboard()
+        {
+            highScores.Add(gameState.Score);
+            highScores.Sort();
+            highScores.Reverse();
+
+            if(highScores.Count > 5) 
+            {
+                highScores.RemoveAt(5);
+            }
+            foreach(var score in highScores) 
+            {
+                OverlayText.Text += $"\n {score}";
+            }
+        }
+        private async Task ShakeWindow(int durationMs)
+        {
+            var oLeft = this.Left;
+            var oTop = this.Top;
+
+            var shakeTimer = new DispatcherTimer(DispatcherPriority.Send);
+
+            shakeTimer.Tick += (sender, args) =>
+            {
+                this.Left = oLeft + random.Next(-10, 11);
+                this.Top = oTop + random.Next(-10, 11);
+            };
+
+            shakeTimer.Interval = TimeSpan.FromMilliseconds(200);
+            shakeTimer.Start();
+
+            await Task.Delay(durationMs);
+            shakeTimer.Stop();
+        }
+    }
+}
